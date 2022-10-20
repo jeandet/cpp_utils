d744eb390eba90d8a11636e27c4a899d03a0e99d
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2022-10-20 10:01:04 +0200

    Supports Catch2 V3+ now
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- tests/containers/test_containers_traits.cpp
+++ tests/containers/test_containers_traits.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 #include <array>
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp>
 #include <containers/traits.hpp>
 #include <deque>
 #include <forward_list>
