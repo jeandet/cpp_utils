463d241b38f60f6df341f33c74c389226766ae56
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2025-01-02 18:02:56 +0100

    Add missing include

--- tests/types/concepts.cpp
+++ tests/types/concepts.cpp
@@ -2,6 +2,7 @@
 #include <catch2/catch_test_macros.hpp>
 #include <types/concepts.hpp>
 #include <vector>
+#include <memory>
 
 #ifdef CPP_UTILS_CONCEPTS_SUPPORTED
 using namespace cpp_utils::types::concepts;
